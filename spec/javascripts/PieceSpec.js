07b4b5bc8d868dfd582372c23536899f3c9d9fa4
Bruno Sanches <brunopsanches@gmail.com>
2013-11-01 01:07:22 -0700

    Delete unused object

--- spec/javascripts/PieceSpec.js
+++ spec/javascripts/PieceSpec.js
@@ -1,10 +1,4 @@
-
 describe("Piece", function() {
-  var piece;
-
-  beforeEach(function() {
-    piece = new Piece();
-  });
 
   it(".shapes describes all 7 initial state piece shapes", function() {
     expect(Piece.shapes.length).toBe(7);
