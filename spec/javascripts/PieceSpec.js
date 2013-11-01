3a83523c1b41d5fb51905e28870d5c26397eff03
Bruno Sanches <brunopsanches@gmail.com>
2013-11-01 00:32:36 -0700

    Group shape tests into "shapes" context

--- spec/javascripts/PieceSpec.js
+++ spec/javascripts/PieceSpec.js
@@ -10,31 +10,33 @@ describe("Piece", function() {
     expect(piece.shapes.length).toBe(7);
   });
 
-  it("first shape represents the T piece", function() {
-    expect(piece.shapes[0]).toEqual([[1, 1, 1], [0, 1, 0]]);
-  });
+  describe("shapes", function() {
+    it("first shape represents the T piece", function() {
+      expect(piece.shapes[0]).toEqual([[1, 1, 1], [0, 1, 0]]);
+    });
 
-  it("second shape represents the square piece", function() {
-    expect(piece.shapes[1]).toEqual([[1, 1], [1, 1]]);
-  });
+    it("second shape represents the square piece", function() {
+      expect(piece.shapes[1]).toEqual([[1, 1], [1, 1]]);
+    });
 
-  it("third shape represents the stick piece", function() {
-    expect(piece.shapes[2]).toEqual([[1], [1], [1], [1]]);
-  });
+    it("third shape represents the stick piece", function() {
+      expect(piece.shapes[2]).toEqual([[1], [1], [1], [1]]);
+    });
 
-  it("fourth shape represents the L piece", function() {
-    expect(piece.shapes[3]).toEqual([[1, 1], [1, 0], [1, 0], [1, 0]]);
-  });
+    it("fourth shape represents the L piece", function() {
+      expect(piece.shapes[3]).toEqual([[1, 1], [1, 0], [1, 0], [1, 0]]);
+    });
 
-  it("fifth shape represents the mirrored L piece", function() {
-    expect(piece.shapes[4]).toEqual([[1, 1], [0, 1], [0, 1], [0, 1]]);
-  });
+    it("fifth shape represents the mirrored L piece", function() {
+      expect(piece.shapes[4]).toEqual([[1, 1], [0, 1], [0, 1], [0, 1]]);
+    });
 
-  it("sixth shape represents the dog piece", function() {
-    expect(piece.shapes[5]).toEqual([[1, 1, 0], [0, 1, 1]]);
-  });
+    it("sixth shape represents the dog piece", function() {
+      expect(piece.shapes[5]).toEqual([[1, 1, 0], [0, 1, 1]]);
+    });
 
-  it("seventh shape represents the mirrored dog piece", function() {
-    expect(piece.shapes[6]).toEqual([[0, 1, 1], [1, 1, 0]]);
+    it("seventh shape represents the mirrored dog piece", function() {
+      expect(piece.shapes[6]).toEqual([[0, 1, 1], [1, 1, 0]]);
+    });
   });
 });
