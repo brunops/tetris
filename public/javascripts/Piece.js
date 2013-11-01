cece4c19419d86740177e7506dc764da41b0989e
Bruno Sanches <brunopsanches@gmail.com>
2013-11-01 00:47:31 -0700

    Define Piece shapes as an attribute for better semantics

--- public/javascripts/Piece.js
+++ public/javascripts/Piece.js
@@ -1,7 +1,7 @@
 function Piece() {
 }
 
-Piece.prototype.shapes = [
+Piece.shapes = [
   [[1, 1, 1], [0, 1, 0]],           // The T
   [[1, 1], [1, 1]],                 // The Square
   [[1], [1], [1], [1]],             // The Stick
--- spec/javascripts/PieceSpec.js
+++ spec/javascripts/PieceSpec.js
@@ -7,36 +7,38 @@ describe("Piece", function() {
   });
 
   it(".shapes describes all 7 initial state piece shapes", function() {
-    expect(piece.shapes.length).toBe(7);
+    expect(Piece.shapes.length).toBe(7);
   });
 
   describe("shapes", function() {
     it("first shape represents the T piece", function() {
-      expect(piece.shapes[0]).toEqual([[1, 1, 1], [0, 1, 0]]);
+      expect(Piece.shapes[0]).toEqual([[1, 1, 1], [0, 1, 0]]);
     });
 
     it("second shape represents the square piece", function() {
-      expect(piece.shapes[1]).toEqual([[1, 1], [1, 1]]);
+      expect(Piece.shapes[1]).toEqual([[1, 1], [1, 1]]);
     });
 
     it("third shape represents the stick piece", function() {
-      expect(piece.shapes[2]).toEqual([[1], [1], [1], [1]]);
+      expect(Piece.shapes[2]).toEqual([[1], [1], [1], [1]]);
     });
 
     it("fourth shape represents the L piece", function() {
-      expect(piece.shapes[3]).toEqual([[1, 1], [1, 0], [1, 0], [1, 0]]);
+      expect(Piece.shapes[3]).toEqual([[1, 1], [1, 0], [1, 0], [1, 0]]);
     });
 
     it("fifth shape represents the mirrored L piece", function() {
-      expect(piece.shapes[4]).toEqual([[1, 1], [0, 1], [0, 1], [0, 1]]);
+      expect(Piece.shapes[4]).toEqual([[1, 1], [0, 1], [0, 1], [0, 1]]);
     });
 
     it("sixth shape represents the dog piece", function() {
-      expect(piece.shapes[5]).toEqual([[1, 1, 0], [0, 1, 1]]);
+      expect(Piece.shapes[5]).toEqual([[1, 1, 0], [0, 1, 1]]);
     });
 
     it("seventh shape represents the mirrored dog piece", function() {
-      expect(piece.shapes[6]).toEqual([[0, 1, 1], [1, 1, 0]]);
+      expect(Piece.shapes[6]).toEqual([[0, 1, 1], [1, 1, 0]]);
     });
   });
+
+
 });
