4dac63a3c2f69fbe98a91a7f2026bfac0eb4524c
Bruno Sanches <brunopsanches@gmail.com>
2013-11-01 00:14:13 -0700

    Define "The T" piece shape

--- public/javascripts/Piece.js
+++ public/javascripts/Piece.js
@@ -2,7 +2,7 @@ function Piece() {
 }
 
 Piece.prototype.shapes = [
-  [],
+  [[1, 1, 1], [0, 1, 0]], // The T
   [],
   [],
   [],
--- spec/javascripts/PieceSpec.js
+++ spec/javascripts/PieceSpec.js
@@ -9,4 +9,8 @@ describe("Piece", function() {
   it(".shapes describes all 7 initial state piece shapes", function() {
     expect(piece.shapes.length).toBe(7);
   });
+
+  it("first shape represents the T piece", function() {
+    expect(piece.shapes[0]).toEqual([[1, 1, 1], [0, 1, 0]]);
+  });
 });
