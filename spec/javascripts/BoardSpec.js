fe1958a49dbb6293354e54da8dc9a2941dbc194c
Bruno Sanches <brunopsanches@gmail.com>
2013-11-04 16:28:09 -0800

    Rewrite test definition

--- spec/javascripts/BoardSpec.js
+++ spec/javascripts/BoardSpec.js
@@ -5,7 +5,7 @@ describe("Board", function() {
     board = new Board();
   });
 
-  it("starts empty, all elements should be zero", function() {
+  it("starts empty (all elements should be zero)", function() {
     var flattenBoard = _.flatten(board.getBody());
 
     expect(_.all(flattenBoard, function(e) { return e === 0 })).toBeTruthy();
