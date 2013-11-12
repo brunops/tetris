a0dfce14859044bb99379ed49375f6b6dc796183
Bruno Sanches <brunopsanches@gmail.com>
2013-11-11 18:31:38 -0800

    Shrink .getFullBodyState() test
    * Create new Board object (object2) and .place() first boards piece into it and compare the results

--- spec/javascripts/BoardSpec.js
+++ spec/javascripts/BoardSpec.js
@@ -48,22 +48,11 @@ describe("Board", function() {
   });
 
   it(".getFullBodyState() returns body with fallingPiece printed on fallingPosition cordinates", function() {
-    var fallingPosition = board.fallingPosition(),
-        startRow        = fallingPosition[0],
-        startCol        = fallingPosition[1],
-        fullBodyState   = board.getFullBodyState();
+    var board2 = new Board();
 
-    for (var row = 0; row < board.getHeight(); ++row) {
-      for (var col = 0; col < board.getWidth(); ++col) {
-        if ((col >= startCol && col < startCol + board.fallingPiece().getWidth()) &&
-            (row >= startRow && row < startRow + board.fallingPiece().getHeight())) {
-          expect(fullBodyState[row][col]).toBe(board.fallingPiece().getBody()[row - startRow][col - startCol]);
-        }
-        else {
-          expect(fullBodyState[row][col]).toBe(board.getBody()[row][col]);
-        }
-      }
-    }
+    board2.place(board.fallingPiece(), board.fallingPosition()[0], board.fallingPosition()[1]);
+
+    expect(board2.getBody()).toEqual(board.getFullBodyState());
   });
 
   xit(".tick() method makes current piece fall 1 square", function() {});
